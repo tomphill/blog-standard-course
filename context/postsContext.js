edc5caf1d4210068aaf649d586ac7fe7baf6e96c
tom <tomphill@live.co.uk>
2023-02-19 14:27:00 +0800

    refactor to useReducer

--- context/postsContext.js
+++ context/postsContext.js
@@ -1,36 +1,51 @@
 import { useRouter } from 'next/router';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useReducer, useState } from 'react';
 
 const PostsContext = React.createContext({});
 
 export default PostsContext;
 
-export const PostsProvider = ({ children }) => {
-  const [posts, setPosts] = useState([]);
-  const [noMorePosts, setNoMorePosts] = useState(false);
-
-  const deletePost = useCallback((postId) => {
-    setPosts((value) => {
-      const newPosts = [];
-      value.forEach((post) => {
-        if (post._id !== postId) {
-          newPosts.push(post);
-        }
-      });
-      return newPosts;
-    });
-  }, []);
-
-  const setPostsFromSSR = useCallback((postsFromSSR = []) => {
-    setPosts((value) => {
-      const newPosts = [...value];
-      postsFromSSR.forEach((post) => {
+function postsReducer(state, action) {
+  switch (action.type) {
+    case 'addPosts': {
+      const newPosts = [...state];
+      action.posts.forEach((post) => {
         const exists = newPosts.find((p) => p._id === post._id);
         if (!exists) {
           newPosts.push(post);
         }
       });
       return newPosts;
+    }
+    case 'deletePost': {
+      const newPosts = [];
+      state.forEach((post) => {
+        if (post._id !== action.postId) {
+          newPosts.push(post);
+        }
+      });
+      return newPosts;
+    }
+    default:
+      return state;
+  }
+}
+
+export const PostsProvider = ({ children }) => {
+  const [posts, dispatch] = useReducer(postsReducer, []);
+  const [noMorePosts, setNoMorePosts] = useState(false);
+
+  const deletePost = useCallback((postId) => {
+    dispatch({
+      type: 'deletePost',
+      postId,
+    });
+  }, []);
+
+  const setPostsFromSSR = useCallback((postsFromSSR = []) => {
+    dispatch({
+      type: 'addPosts',
+      posts: postsFromSSR,
     });
   }, []);
 
@@ -48,15 +63,9 @@ export const PostsProvider = ({ children }) => {
       if (postsResult.length < 5) {
         setNoMorePosts(true);
       }
-      setPosts((value) => {
-        const newPosts = [...value];
-        postsResult.forEach((post) => {
-          const exists = newPosts.find((p) => p._id === post._id);
-          if (!exists) {
-            newPosts.push(post);
-          }
-        });
-        return newPosts;
+      dispatch({
+        type: 'addPosts',
+        posts: postsResult,
       });
     },
     []
